0b0570b4a89ddb4c8c2a35916a3634f1025f97bd
Nipuni Wimangsa <nipuniwimangsa@gmail.com>
2024-03-22 17:16:40 +0530

    finishing welcome page component

--- LibraryManagementSystem/src/app/app.module.ts
+++ LibraryManagementSystem/src/app/app.module.ts
@@ -8,11 +8,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatRippleModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AppComponent, WelcomePageComponent],
-  imports: [BrowserModule, AppRoutingModule, MatButtonModule],
+  imports: [BrowserModule, AppRoutingModule, MatRippleModule, MatButtonModule],
   providers: [provideClientHydration(), provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
