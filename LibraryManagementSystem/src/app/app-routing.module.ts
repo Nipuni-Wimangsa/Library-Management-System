33541d1571208ae96ea9ace732a9f14eb5c75ca4
Nipuni Wimangsa <nipuniwimangsa@gmail.com>
2024-04-23 07:31:54 +0530

    latest small changes

--- LibraryManagementSystem/src/app/app-routing.module.ts
+++ LibraryManagementSystem/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { AdminHeaderComponent } from './admin-header/admin-header.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'welcomePage', pathMatch: 'full' },
   { path: 'welcomePage', component: WelcomePageComponent },
   { path: 'login', component: LoginComponent },
   { path: 'adminHome', component: AdminHomeComponent },
+  { path: 'adminHeader', component: AdminHeaderComponent}
 ];
 
 @NgModule({
